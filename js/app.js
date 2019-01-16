0151e1a5aa4a4fd14a744e005192220f3bb1eed4
John <ema8720@gmail.com>
2019-01-15 21:33:02 -0800

    working on navigation

--- js/app.js
+++ js/app.js
@@ -7,6 +7,23 @@ filterBtn.forEach(function(btn){
     btn.addEventListener('click', function(e){
         e.preventDefault();
         const value = e.target.dataset.filter
+        // console.log(value);
+        const items = document.querySelectorAll('.store-item');
+        // console.log(items);
+        items.forEach(function(item){
+            if (value === 'all'){
+                item.style.display = 'block';
+            }
+            else {
+                if (item.classList.contains(value)){
+                    item.style.display = 'block';
+                }
+                else {
+                    item.style.display = 'none';
+                }
+            }
+        })
+        
     })
 })
 
